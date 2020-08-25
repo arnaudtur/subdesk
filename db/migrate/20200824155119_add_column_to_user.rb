6a8fc1f31368e225994da2e0be459f65cc6ec2b4
Arnaud Turnani <arnaud.tur@live.fr>
2020-08-25 11:44:46 +0200

    rework model

--- db/migrate/20200824155119_add_column_to_user.rb
+++ db/migrate/20200824155119_add_column_to_user.rb
@@ -6,9 +6,12 @@ class AddColumnToUser < ActiveRecord::Migration[6.0]
     add_column :users, :year_of_creation, :integer
     add_column :users, :number_of_employees, :integer
     add_column :users, :average_age, :integer
-    add_column :users, :weblinks, :string
+    add_column :users, :weblink, :string
     add_column :users, :field, :string
     add_column :users, :code_naf, :string
-    add_column :users, :criteria, :string, array: true, default: []
+    add_column :users, :criteria1, :integer
+    add_column :users, :criteria2, :integer
+    add_column :users, :criteria3, :integer
+    add_column :users, :criteria4, :integer
   end
 end
