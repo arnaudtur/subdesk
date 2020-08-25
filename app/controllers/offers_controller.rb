68e4166807dff3735da00895a67c985da2da8491
Arnaud Turnani <arnaud.tur@live.fr>
2020-08-25 17:11:45 +0200

    Pundit + Offer Create

---
+++ app/controllers/offers_controller.rb
@@ -0,0 +1,26 @@
+class OffersController < ApplicationController
+  def new
+    @offer = Offer.new
+    authorize @offer
+  end
+
+  def create
+    @offer = Offer.new(params_offers)
+    @user = current_user
+    @offer.user = @user
+    @offer.status = "Pending"
+    authorize @offer
+    if @offer.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+
+  private
+
+  def params_offers
+    params.require(:offer).permit(:status, :name, :price, :address, :description, :price, :total_seats, :seats, :room, :size, :question1, :question2, :question3)
+  end
+end
