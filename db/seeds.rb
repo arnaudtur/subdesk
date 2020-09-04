867107fe6af5b3795dfb7a03ddb776b5c195d670
Arnaud Turnani <arnaud.tur@live.fr>
2020-09-04 10:07:54 +0200

    fix seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 require "open-uri"
-
+Notification.destroy_all
 Message.destroy_all
 Chatroom.destroy_all
 Booking.destroy_all
